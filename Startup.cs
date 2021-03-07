ac18cddedc5d72de5204ece7d083fc8e0b4f9ce0
“Chaipetchp13” <Chaipetch.p@outlook.co.th>
2021-03-07 23:16:07 +0700

    Change Sqlite to Postgresql

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace DotnetIdentity
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseNpgsql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
